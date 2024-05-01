0df609f79230422adc280d7ca41127ecb8ad1793
Jagadabi-Varshitha <163122652+Jagadabi-Varshitha@users.noreply.github.com>
2024-05-01 22:27:13 +0530

    Add files via upload

---
+++ task7.py
@@ -0,0 +1,14 @@
+def can_form_palindrome(s):
+    char_set = set()
+    for char in s:
+        if char in char_set:
+            char_set.remove(char)
+        else:
+            char_set.add(char)
+    if len(char_set) <= 1:
+        return "YES"
+    else:
+        return "NO"
+s = input()
+result = can_form_palindrome(s)
+print(result)
